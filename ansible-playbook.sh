fdb7fedf6a02ca4ebfe18dc21fc7aecfc347553b
Madan Kumar <smadan2703@gmail.com>
2017-12-06 15:49:19 +0530

    Ansible Script to create Playbook structure

---
+++ ansible-playbook.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+
+if [ "$#" -eq 0 ]; then
+    echo -e "Script must run with atleast 1 parameter to create ansible roles\nFor example ./ansible-playbook.sh httpd sonar \n"
+    exit 1
+fi
+
+if [ ! -d "playbook" ]; then
+    mkdir -p playbook/roles
+    cd playbook    
+    touch {main.yml,hosts}
+
+    for rolename in "$@"
+    do
+        mkdir -p roles/$rolename/{tasks,templates,vars,handlers}
+        touch roles/$rolename/{tasks,templates,vars,handlers}/main.yml
+    done
+else
+    cd playbook
+    for rolename in "$@"
+    do
+        mkdir -p roles/$rolename/{tasks,templates,vars,handlers}
+        touch roles/$rolename/{tasks,templates,vars,handlers}/main.yml
+    done
+fi
